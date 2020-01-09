57c2d0dde4c16176244772d8e60ef1d4cef29c72
Muhammad Shamsul Maruf <smaruf00ruet320@gmail.com>
2020-01-09 12:25:43 +0600

    Create prototyping class 2020.01.09.1225

---
+++ src/main/java/sample/Prototype.java
@@ -0,0 +1,11 @@
+public class Prototype {
+  public static void main(String[] args) {
+      init();
+      buyTicket();
+      drawTicket();
+      calculateRanks();
+      calculatePrizeMoney();
+      claimTicket();
+      calculateProfit();
+  }
+}
